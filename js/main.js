6c88564b494466c94a3c9633028ee0ab2a2f0213
tenglei.ning <tlning@i9i8.com>
2019-11-25 16:12:58 +0800

    运营管理

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 var metakeywords = {
-    'oymm': 'oymm,偶遇妹妹,偶遇美眉,偶遇猫咪,欧亚木门,欧亚猫咪',
+    'oymm': 'oymm,欧阳明明,欧阳菲菲,偶遇妹妹,偶遇美眉,偶遇猫咪,欧亚木门,欧亚猫咪',
     'mkvs': 'MKVS,MKVS-shop,MKVS-org,MKVS-company',
 }
 
 var metadescription = {
-    'oymm': 'oymm,经典四字符单词.com域名,偶遇美眉,欧亚木门,偶遇猫咪',
+    'oymm': 'oymm,欧阳明明,欧阳菲菲,偶遇美眉,欧亚木门,偶遇猫咪',
     'mkvs': 'MKVS-company MKVS-img mkvs for mkv',
 }
