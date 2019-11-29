a80863fb21d1c5d79c67a84c40b3e459bb4faa35
tenglei.ning <tlning@i9i8.com>
2019-11-29 12:03:47 +0800

    title

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 var metakeywords = {
     'oymm': 'oymm,欧阳明明,欧阳菲菲,偶遇妹妹,偶遇美眉,偶遇猫咪,欧亚木门,欧亚猫咪',
     'mkvs': 'MKVS,MKVS-shop,MKVS-org,MKVS-company',
+    'fvbook': 'FV书,fvboox,FV电器书,fv详解书,fv book,fv小说',
 }
 
 var metadescription = {
     'oymm': 'oymm,欧阳明明,欧阳菲菲,偶遇美眉,欧亚木门,偶遇猫咪',
     'mkvs': 'MKVS-company MKVS-img mkvs for mkv',
+    'fvbook': 'fv for book,fv书本,fv读书网,fv小说网,fv书网',
 }
